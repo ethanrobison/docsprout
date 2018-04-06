94839a63b03a40b104c845aff9d0d83931c4b475
Ethan <ethanrobison@u.northwestern.edu>
2018-04-06 14:31:51 -0500

    platform manager

---
+++ Assets/Code/Session/PlatformManager.cs
@@ -0,0 +1,22 @@
+﻿namespace Code.Session
+{
+    /// <summary>
+    /// Handles cross-platform stuff.
+    /// Lots of nasty stuff, but at least it's black-boxed.
+    /// </summary>
+    public class PlatformManager : ISessionManager
+    {
+        private enum Platform
+        {
+            OSX,
+            Windows,
+            Linux
+        }
+
+        private Platform _platform;
+
+        public void Initialize () { }
+
+        public void ShutDown () { }
+    }
+}
