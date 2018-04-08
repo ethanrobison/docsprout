20421f649dcd55a7bfd61eaa8f430a615cfbe692
Kyle Engelmann <kyleengelmann2018@u.northwestern.edu>
2018-04-08 11:16:49 -0500

    removed some comments

--- Assets/Code/Characters/Character.cs
+++ Assets/Code/Characters/Character.cs
@@ -11,29 +11,11 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class Character : MonoBehaviour {
     
-    /// <summary>
-    /// The character's character controller.
-    /// </summary>
     CharacterController characterController;
 
-    /// <summary>
-    /// The character's velocity. The character moves every physics update based off the velocity
-    /// </summary>
     [HideInInspector] public Vector3 velocity = Vector3.zero;
-
-    /// <summary>
-    /// Whether or not the character is on the ground
-    /// </summary>
     [HideInInspector] public bool isOnGround;
-
-    /// <summary>
-    /// The collision flags returned after moving the Character
-    /// </summary>
     [HideInInspector] public CollisionFlags collisionFlags;
-
-    /// <summary>
-    /// The surface normal of the ground the chracter is standing on
-    /// </summary>
     [HideInInspector] public Vector3 groundNormal = Vector3.up;
 
 
--- Assets/Code/Characters/Walk.cs
+++ Assets/Code/Characters/Walk.cs
@@ -13,44 +13,14 @@ using UnityEngine;
 [RequireComponent(typeof(Character))]
 public class Walk : MonoBehaviour {
 
-    /// <summary>
-    /// The maximum speed the character can move at
-    /// </summary>
     public float maxSpeed = 5f;
-
-    /// <summary>
-    /// The acceleration of the character while speeding up
-    /// </summary>
     public float speedUpAcceleration = 30f;
-
-    /// <summary>
-    /// The acceleration of the character while slowing down
-    /// </summary>
     public float slowDownAcceleration = 40f;
-
-    /// <summary>
-    /// The acceleration of the character while switching directions
-    /// </summary>
     public float switchDirectionsAcceleration = 50f;
 
-
-
-    /// <summary>
-    /// The attached character component.
-    /// </summary>
     Character character;
-
-    /// <summary>
-    /// Is the character switching directions?
-    /// </summary>
     bool isSwitchingDirs;
 
-
-    /// <summary>
-    /// The direction in world space that the character should walk in. Every physics
-    /// update this is used to calculate the character's velocity, with magnitudes of
-    /// 1 or greater resulting in the character accelerating towards max speed.
-    /// </summary>
     [HideInInspector] public Vector2 walkingDir;
 
 
@@ -60,76 +30,74 @@ public class Walk : MonoBehaviour {
 
 	void FixedUpdate()
 	{
-        if(character.isOnGround) {
-            // Calculate how fast the character should aim to be moving
-            float walkMagnitude = Mathf.Min(walkingDir.magnitude, 1);
-            float goalSpeed = walkMagnitude*maxSpeed;
+        // Calculate how fast the character should aim to be moving
+        float walkMagnitude = Mathf.Min(walkingDir.magnitude, 1);
+        float goalSpeed = walkMagnitude*maxSpeed;
 
-            // Find the goal velocity, ensuring it is parallel to the ground
-            Vector3 goalWalkingDir = new Vector3(walkingDir.x, 0f, walkingDir.y);
-            goalWalkingDir -= Vector3.Dot(goalWalkingDir, character.groundNormal)*character.groundNormal;
-            goalWalkingDir = goalWalkingDir.normalized;
+        // Find the goal velocity, ensuring it is parallel to the ground
+        Vector3 goalWalkingDir = new Vector3(walkingDir.x, 0f, walkingDir.y);
+        goalWalkingDir -= Vector3.Dot(goalWalkingDir, character.groundNormal)*character.groundNormal;
+        goalWalkingDir = goalWalkingDir.normalized;
 
-            // Find the component of the character's velocity parallel and perpendicular to the ground
-            Vector3 slopedVelocity = character.velocity;
-            float normalVelocity = Vector3.Dot(character.velocity, character.groundNormal); // perpendicular to slope
-            slopedVelocity -= normalVelocity*character.groundNormal; // parallel to slope
+        // Find the component of the character's velocity parallel and perpendicular to the ground
+        Vector3 slopedVelocity = character.velocity;
+        float normalVelocity = Vector3.Dot(character.velocity, character.groundNormal); // perpendicular to slope
+        slopedVelocity -= normalVelocity*character.groundNormal; // parallel to slope
 
-            // The character's speed in the goal direction
-            float parallelSpeed = Vector3.Dot(slopedVelocity, goalWalkingDir);
+        // The character's speed in the goal direction
+        float parallelSpeed = Vector3.Dot(slopedVelocity, goalWalkingDir);
 
-            // Find the direction and speed perpendicular to the goal direction
-            Vector3 perpendicularDir = slopedVelocity - goalWalkingDir*parallelSpeed;
-            float perpendicularSpeed = perpendicularDir.magnitude;
-            perpendicularDir /= perpendicularSpeed + 0.000001f;
+        // Find the direction and speed perpendicular to the goal direction
+        Vector3 perpendicularDir = slopedVelocity - goalWalkingDir*parallelSpeed;
+        float perpendicularSpeed = perpendicularDir.magnitude;
+        perpendicularDir /= perpendicularSpeed + 0.000001f;
 
-            // Find the change in velocity to use when slowing down now so it doesn't
-            // need to be calculated twice if the character is slowing down, since it
-            // is also used to cancel out the perpendicular velocity
-            float slowingDV = slowDownAcceleration*Time.fixedDeltaTime;
+        // Find the change in velocity to use when slowing down now so it doesn't
+        // need to be calculated twice if the character is slowing down, since it
+        // is also used to cancel out the perpendicular velocity
+        float slowingDV = slowDownAcceleration*Time.fixedDeltaTime;
 
-            // Find out whether or not we are switching directions
-            if(!isSwitchingDirs) isSwitchingDirs = parallelSpeed/slopedVelocity.magnitude < -0.1f;
-            if(isSwitchingDirs) { // Switching directions
-                float dV = switchDirectionsAcceleration*Time.fixedDeltaTime;
+        // Find out whether or not we are switching directions
+        if(!isSwitchingDirs) isSwitchingDirs = parallelSpeed/slopedVelocity.magnitude < -0.1f;
+        if(isSwitchingDirs) { // Switching directions
+            float dV = switchDirectionsAcceleration*Time.fixedDeltaTime;
 
-                // Check if we have finished switching directions and also prevent overcorrection
-                if(dV > goalSpeed - parallelSpeed) {
-                    parallelSpeed = goalSpeed;
-                    isSwitchingDirs = false;
-                }
-                else {
-                    parallelSpeed += dV;
-                }
-            }
-            else if(parallelSpeed < goalSpeed) { // Speeding up
-                parallelSpeed += speedUpAcceleration*Time.fixedDeltaTime;
-                parallelSpeed = Mathf.Min(parallelSpeed, goalSpeed); // prevent overcorrection
-            }
-            else { // Slowing down
-                if(slowingDV > goalSpeed - parallelSpeed) { // prevent overcorrection
-                    parallelSpeed = goalSpeed;
-                }
-                else {
-                    parallelSpeed -= slowingDV;
-                }
-
-            }
-
-            // Velocity perpendicular to the goal direction is always slowing down
-            float perpSign = Mathf.Sign(perpendicularSpeed);
-
-            if(slowingDV > perpendicularSpeed*perpSign) {
-                perpendicularSpeed = 0;
+            // Check if we have finished switching directions and also prevent overcorrection
+            if(dV > goalSpeed - parallelSpeed) {
+                parallelSpeed = goalSpeed;
+                isSwitchingDirs = false;
             }
             else {
-                perpendicularSpeed -= slowingDV*perpSign;
+                parallelSpeed += dV;
+            }
+        }
+        else if(parallelSpeed < goalSpeed) { // Speeding up
+            parallelSpeed += speedUpAcceleration*Time.fixedDeltaTime;
+            parallelSpeed = Mathf.Min(parallelSpeed, goalSpeed); // prevent overcorrection
+        }
+        else { // Slowing down
+            if(slowingDV > goalSpeed - parallelSpeed) { // prevent overcorrection
+                parallelSpeed = goalSpeed;
+            }
+            else {
+                parallelSpeed -= slowingDV;
             }
 
-            // Combine all components of velocity
-            character.velocity = parallelSpeed*goalWalkingDir + perpendicularSpeed*perpendicularDir + normalVelocity*character.groundNormal;
-
         }
+
+        // Velocity perpendicular to the goal direction is always slowing down
+        float perpSign = Mathf.Sign(perpendicularSpeed);
+
+        if(slowingDV > perpendicularSpeed*perpSign) {
+            perpendicularSpeed = 0;
+        }
+        else {
+            perpendicularSpeed -= slowingDV*perpSign;
+        }
+
+        // Combine all components of velocity
+        character.velocity = parallelSpeed*goalWalkingDir + perpendicularSpeed*perpendicularDir + normalVelocity*character.groundNormal;
+
 
         // Apply gravity
         character.velocity += Physics.gravity*Time.fixedDeltaTime;
