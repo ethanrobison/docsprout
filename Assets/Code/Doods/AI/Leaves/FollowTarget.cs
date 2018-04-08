20ce9f2e13f87814760bb642d1d86bf01f37e35e
Ethan <ethanrobison@u.northwestern.edu>
2018-04-08 18:28:16 -0500

    follow target shittily

---
+++ Assets/Code/Doods/AI/Leaves/FollowTarget.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+namespace Code.Doods.AI {
+	public class FollowTarget<T> : BehaviorTreeNode where T : MonoBehaviour {
+		int _mask;
+		float _thresh;
+
+		Transform _target;
+
+		/// <summary>
+		/// Follows targets of type T that come within thresh of the dood.
+		/// </summary>
+		public FollowTarget (Dood dood, int layermask, float thresh = 10f) : base (dood)
+		{
+			_mask = layermask;
+			_thresh = thresh;
+		}
+
+		//Collider [] _results = new Collider [4];
+		public override void OnInitialize ()
+		{
+			base.OnInitialize ();
+			Debug.Assert (_target == null, "Already have target!");
+
+			var _results = Physics.OverlapSphere (_dood.transform.position, _thresh, _mask);
+			if (_results.Length > 0) {
+				var go = _results [0].gameObject;
+				if (go == null || go.GetComponent<T> () == null) {
+					_target = null;
+					return;
+				}
+				_target = go.transform;
+			}
+		}
+
+		public override void OnTerminate (Status result)
+		{
+			base.OnTerminate (result);
+			_target = null;
+		}
+
+		protected override Status Update ()
+		{
+			if (_target == null) { return Status.Failure; }
+			if (_dood.MoveTowards (_target.position)) { return Status.Success; }
+			return Status.Running;
+		}
+	}
+}
--- Assets/Code/Doods/Dood.cs
+++ Assets/Code/Doods/Dood.cs
@@ -10,7 +10,11 @@ namespace Code.Doods {
 
 		public void Initialize ()
 		{
+			int layermask = LayerMask.GetMask ("Player");
+			var follow = new FollowTarget<Player> (this, layermask);
+
 			var seq = new Sequence (this);
+			seq.AddChild (follow);
 			seq.AddChild (new LogMessage ("Hello", this));
 			seq.AddChild (new Idle (this));
 			Behavior = new Root (seq, this);
@@ -24,6 +28,29 @@ namespace Code.Doods {
 		void Update ()
 		{
 			var status = Behavior.Tick ();
+			switch (status) {
+			case Status.Invalid:
+				Debug.Log ("Invalid");
+				break;
+			case Status.Running:
+				Debug.Log ("Running");
+				break;
+			case Status.Success:
+				Debug.Log ("Success");
+				break;
+			case Status.Failure:
+				Debug.Log ("Failure");
+				break;
+			}
+		}
+
+
+		public bool MoveTowards (Vector3 pos, float thresh = 1f)
+		{
+			if (Vector3.Distance (pos, transform.position) < thresh) { return true; }
+			var direction = (pos - transform.position).normalized;
+			_walk.SetDir (direction);
+			return false;
 		}
 	}
 }
