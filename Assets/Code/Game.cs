4cb2270737d1d318d85f257e5c55e0aa9b9def84
Ethan <ethanrobison@u.northwestern.edu>
2018-04-08 12:13:43 -0500

    fix main menu lighting

--- Assets/Code/Game.cs
+++ Assets/Code/Game.cs
@@ -2,27 +2,27 @@
 using Code.Utils;
 using UnityEngine;
 
-namespace Code
-{
-    public class Game : MonoBehaviour
-    {
-        public static GameContext Ctx;
-        public static GameSession Sesh;
+namespace Code {
+	public class Game : MonoBehaviour {
+		public static GameContext Ctx;
+		public static GameSession Sesh;
 
-        private void Start () {
-            // todo I am a singleton; if I already exist, kill the Buddha
+		private void Start ()
+		{
+			// todo I am a singleton; if I already exist, kill the Buddha
 
-            DontDestroyOnLoad(gameObject); // Please keep me kthxbai.
-            DontDestroyOnLoad(UIUtils.GetCanvas().gameObject);
-            DontDestroyOnLoad(GameObject.Find("EventSystem")); // hard-coding is best coding
+			DontDestroyOnLoad (gameObject); // Please keep me kthxbai.
+			DontDestroyOnLoad (UIUtils.GetCanvas ().gameObject);
+			DontDestroyOnLoad (GameObject.Find ("EventSystem")); // hard-coding is best coding
 
-            Sesh = new GameSession();
-            Sesh.Initialize();
-        }
+			Sesh = new GameSession ();
+			Sesh.Initialize ();
+		}
 
-        public static void SetContext (GameSession sesh, GameContext ctx) {
-            Debug.Assert(sesh == Sesh, "Wrong session trying to add context?");
-            Ctx = ctx;
-        }
-    }
+		public static void SetContext (GameSession sesh, GameContext ctx)
+		{
+			Debug.Assert (sesh == Sesh, "Wrong session trying to add context?");
+			Ctx = ctx;
+		}
+	}
 }
