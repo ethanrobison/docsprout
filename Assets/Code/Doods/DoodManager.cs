0b36dd8029aa11760cb21aa33692a41d552460fe
Ethan <ethanrobison@u.northwestern.edu>
2018-04-08 13:54:56 -0500

    make sure to load the relevant stuff _after_ the scene loads

--- Assets/Code/Doods/DoodManager.cs
+++ Assets/Code/Doods/DoodManager.cs
@@ -1,9 +1,27 @@
-﻿namespace Code.Doods
-{
-    public class DoodManager : IContextManager
-    {
-        public void Initialize () { }
+﻿using System.Collections.Generic;
+using UnityEngine;
 
-        public void ShutDown () { }
-    }
+namespace Code.Doods {
+	public class DoodManager : IContextManager {
+		static Object Prefab;
+
+		readonly List<Dood> _doods = new List<Dood> ();
+
+		public void Initialize ()
+		{
+			Prefab = Resources.Load ("Dood");
+			MakeDood ();
+		}
+
+		public void ShutDown () { }
+
+
+		void MakeDood ()
+		{
+			var go = (GameObject)Object.Instantiate (Prefab);
+			var dood = go.GetComponent<Dood> ();
+
+			_doods.Add (dood);
+		}
+	}
 }
--- Assets/Code/GameContext.cs
+++ Assets/Code/GameContext.cs
@@ -2,41 +2,45 @@
 using Code.UI;
 using UnityEngine.SceneManagement;
 
-namespace Code
-{
-    public class GameContext
-    {
-        public DialogManager Dialogs { get; private set; }
-        public DoodManager Doods { get; private set; }
+namespace Code {
+	public class GameContext {
+		public DialogManager Dialogs { get; private set; }
+		public DoodManager Doods { get; private set; }
 
-        public void StartGame (int scene) {
-            SceneManager.LoadScene(scene);
+		public void StartGame (int scene)
+		{
+			// todo we should care which scene gets loaded
+			SceneManager.sceneLoaded += (foo, bar) => {
+				Initialize ();
+			};
+			SceneManager.LoadScene (scene);
 
-            Initialize();
-        }
+			Initialize ();
+		}
 
-        private void Initialize () {
-            Dialogs = new DialogManager();
-            Doods = new DoodManager();
+		void Initialize ()
+		{
+			Dialogs = new DialogManager ();
+			Doods = new DoodManager ();
 
-            Dialogs.Initialize();
-            Doods.Initialize();
-        }
+			Dialogs.Initialize ();
+			Doods.Initialize ();
+		}
 
-        private void ShutDown () {
-            Doods.ShutDown();
-            Dialogs.ShutDown();
+		void ShutDown ()
+		{
+			Doods.ShutDown ();
+			Dialogs.ShutDown ();
 
-            Doods = null;
-            Dialogs = null;
-        }
+			Doods = null;
+			Dialogs = null;
+		}
 
-        // todo exit a game and return to the main menu
-    }
+		// todo exit a game and return to the main menu
+	}
 
-    public interface IContextManager
-    {
-        void Initialize ();
-        void ShutDown ();
-    }
+	public interface IContextManager {
+		void Initialize ();
+		void ShutDown ();
+	}
 }
