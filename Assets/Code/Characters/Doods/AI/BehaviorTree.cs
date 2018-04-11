7320f8c31606106df792b87d91cc4cabad2f43a4
Ethan <ethanrobison@u.northwestern.edu>
2018-04-10 21:14:37 -0500

    turn behavior tree into its own component

--- Assets/Code/Characters/Doods/AI/BehaviorTree.cs
+++ Assets/Code/Characters/Doods/AI/BehaviorTree.cs
@@ -1,10 +1,44 @@
-﻿using UnityEngine;
+﻿using Code.Characters.Player;
+using UnityEngine;
 
 namespace Code.Doods.AI {
+	[RequireComponent (typeof (Dood))]
 	public class BehaviorTree : MonoBehaviour {
-		
-		public void Tick(){
-			
+		public Root Root { get; private set; }
+
+		void Start ()
+		{
+			var dood = GetComponent<Dood> ();
+			var follow = new FollowTarget<Player> (dood,
+												   LayerMask.GetMask ("Player"),
+												   3);
+			int layermask = LayerMask.GetMask ("Player");
+
+			var seq = new Sequence (dood);
+			seq.AddChild (follow);
+			seq.AddChild (new LogMessage ("Hello", dood));
+			seq.AddChild (new Idle (dood));
+
+			var sel = new Selector (dood);
+			sel.AddChild (follow);
+			sel.AddChild (new Idle (dood));
+
+			Root = new Root (sel, dood);
+		}
+
+		void Update ()
+		{
+			var status = Root.Tick ();
+			switch (status) {
+			case Status.Invalid:
+				break;
+			case Status.Running:
+				break;
+			case Status.Success:
+				break;
+			case Status.Failure:
+				break;
+			}
 		}
 	}
 }
