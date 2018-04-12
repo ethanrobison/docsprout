0936e919ad3fbe3c23d44c7697514cb6853d80dc
Kyle Engelmann <kyleengelmann2018@u.northwestern.edu>
2018-04-12 01:28:45 -0500

    screendoor transparency on trees when camera gets close

--- Assets/Code/Characters/Player/CameraController.cs
+++ Assets/Code/Characters/Player/CameraController.cs
@@ -1,80 +1,110 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class CameraController : MonoBehaviour {
+namespace Code.Characters.Player {
+	public class CameraController : MonoBehaviour {
 
-	public Camera camera;
-	public Transform target;
-	float camRotY = 30f;
-	float camRotX;
+		public Camera camera;
+		public Transform target;
+		float camRotY = 30f;
+		float camRotX;
 
-	public LayerMask obscuresCamera;
+		public LayerMask obscuresCamera;
 
-	public float xSensitivity = 100f;
-	public float ySensitivity = 50f;
-	public float followDistance = 16f;
-	public float minYAngle = -20f;
-	public float maxYAngle = 50f;
-	public bool invertY = false;
-	public float stiffness = 25f;
+		public float xSensitivity = 100f;
+		public float ySensitivity = 50f;
+		public float followDistance = 16f;
+		public float minYAngle = -20f;
+		public float maxYAngle = 50f;
+		public bool invertY = false;
+		public float stiffness = 25f;
+		public float AlphaSlope = 5f;
+
+		RaycastHit [] alphaHits;
+		// Use this for initialization
+		void Start ()
+		{
+			if (target == null) target = transform;
+			Quaternion goalCamRot = target.rotation * Quaternion.AngleAxis (camRotX, Vector3.up);
+			goalCamRot *= Quaternion.AngleAxis (camRotY, Vector3.right);
+
+			RaycastHit hit;
+			float camDist = followDistance;
+			if (Physics.SphereCast (target.position, .5f, -(goalCamRot * Vector3.forward), out hit, followDistance - .5f, obscuresCamera)) {
+				camDist = hit.distance;
+			}
+
+			Vector3 goalCamPos = target.position - goalCamRot * Vector3.forward * camDist;
 
 
-	// Use this for initialization
-	void Start ()
-	{
-		if (target == null) target = transform;
-		Quaternion goalCamRot = target.rotation * Quaternion.AngleAxis (camRotX, Vector3.up);
-		goalCamRot *= Quaternion.AngleAxis (camRotY, Vector3.right);
+			camera.transform.position = goalCamPos;
+			camera.transform.rotation = goalCamRot;
 
-		RaycastHit hit;
-		float camDist = followDistance;
-		if (Physics.SphereCast (target.position, .5f, -(goalCamRot * Vector3.forward), out hit, followDistance - .5f, obscuresCamera)) {
-			camDist = hit.distance;
+			alphaHits = new RaycastHit [8];
+
 		}
 
-		Vector3 goalCamPos = target.position - goalCamRot * Vector3.forward * camDist;
+		// Update is called once per frame
+		public void moveCamera (float x, float y)
+		{
 
+			camRotX += x * xSensitivity;
+			camRotX = camRotX % 360;
 
-		camera.transform.position = goalCamPos;
-		camera.transform.rotation = goalCamRot;
+			float dYCam = y;
+			if (!invertY) dYCam *= -1;
 
-	}
+			camRotY += dYCam * ySensitivity;
 
-	// Update is called once per frame
-	public void moveCamera (float x, float y)
-	{
+			camRotY = Mathf.Clamp (camRotY, minYAngle, maxYAngle);
 
-		camRotX += x * xSensitivity;
-		camRotX = camRotX % 360;
-
-		float dYCam = y;
-		if (!invertY) dYCam *= -1;
-
-		camRotY += dYCam * ySensitivity;
-
-		camRotY = Mathf.Clamp (camRotY, minYAngle, maxYAngle);
-
-	}
-
-	void FixedUpdate ()
-	{
-		Quaternion goalCamRot = target.rotation * Quaternion.AngleAxis (camRotX, Vector3.up);
-		goalCamRot *= Quaternion.AngleAxis (camRotY, Vector3.right);
-
-		RaycastHit hit;
-		float camDist = followDistance;
-		if (Physics.SphereCast (target.position, .25f, -(goalCamRot * Vector3.forward), out hit, followDistance - .25f, obscuresCamera, QueryTriggerInteraction.Ignore)) {
-			camDist = hit.distance;
 		}
 
-		Vector3 goalCamPos = target.position - goalCamRot * Vector3.forward * camDist;
+		Environment.ScreenDoorTransparency lastSet;
+		void Update ()
+		{
+			float camDist = followDistance;
+			int numHit = Physics.RaycastNonAlloc (camera.transform.position - camera.transform.forward, camera.transform.forward, alphaHits,
+											 Vector3.Distance (camera.transform.position, target.position), ~obscuresCamera.value,
+											 QueryTriggerInteraction.Ignore);
+
+			Environment.ScreenDoorTransparency sdt = null;
+			float dist = 0;
+			for (int i = 0; i < numHit; ++i) {
+				if (sdt = alphaHits [i].collider.gameObject.GetComponent<Environment.ScreenDoorTransparency> ()) {
+					dist = alphaHits [i].distance;
+					break;
+				}
+			}
+			if (lastSet) {
+				lastSet.Alpha = 1f;
+			}
+			if (sdt) {
+				sdt.Alpha = dist / (AlphaSlope + 0.0001f);
+			}
+			lastSet = sdt;
+
+		}
+
+		void FixedUpdate ()
+		{
+			Quaternion goalCamRot = target.rotation * Quaternion.AngleAxis (camRotX, Vector3.up);
+			goalCamRot *= Quaternion.AngleAxis (camRotY, Vector3.right);
+
+			RaycastHit hit;
+			float camDist = followDistance;
+			if (Physics.SphereCast (target.position, .25f, -(goalCamRot * Vector3.forward), out hit, followDistance - .25f, obscuresCamera, QueryTriggerInteraction.Ignore)) {
+				camDist = hit.distance;
+			}
+
+			Vector3 goalCamPos = target.position - goalCamRot * Vector3.forward * camDist;
 
 
-		camera.transform.position = Vector3.Lerp (camera.transform.position, goalCamPos, stiffness * Time.fixedDeltaTime);
-		camera.transform.rotation = Quaternion.Slerp (camera.transform.rotation, goalCamRot, stiffness * Time.fixedDeltaTime);
+			camera.transform.position = Vector3.Lerp (camera.transform.position, goalCamPos, stiffness * Time.fixedDeltaTime);
+			camera.transform.rotation = Quaternion.Slerp (camera.transform.rotation, goalCamRot, stiffness * Time.fixedDeltaTime);
 
 
 
+		}
 	}
+
 }
--- Assets/Resources/Materials/Shaders/celDiffuse.shader
+++ Assets/Resources/Materials/Shaders/celDiffuse.shader
@@ -3,8 +3,7 @@
 		_Color ("Color", Color) = (1,1,1,1)
 		_MainTex ("Albedo (RGB)", 2D) = "white" {}
         _LightMultiplier ("Light Multiplier", Float) = .7
-        _PixelSkip ("Pixel Skip", Float) = 0
-        _PixelSize ("Pixel Size", Float) = 1
+        _Alpha ("Alpha", Float) = 1
 	}
 	SubShader {
 		Tags { "RenderType"="Opaque" }
@@ -17,16 +16,7 @@
 		// Use shader model 3.0 target, to get nicer looking lighting
 		#pragma target 3.0
 
-        float _LightMultiplier;
-
-        half4 LightingCel (SurfaceOutput s, half3 lightDir, half atten) {
-            half NdotL = dot (s.Normal, lightDir);
-            NdotL = min(1, max(0, NdotL)*20)*_LightMultiplier;
-            half4 c;
-            c.rgb = s.Albedo * _LightColor0.rgb * (NdotL * atten);
-            c.a = s.Alpha;
-            return c;
-        }
+        #include "celLighting.cginc"
 
         float2 _ScreenSize;
         float _PixelSize;
@@ -43,19 +33,21 @@
 		UNITY_INSTANCING_BUFFER_START(Props)
 			UNITY_DEFINE_INSTANCED_PROP(fixed4, _Color)
             UNITY_DEFINE_INSTANCED_PROP(sampler2D, _MainTex)
-            UNITY_DEFINE_INSTANCED_PROP(float, _PixelSkip)
+            UNITY_DEFINE_INSTANCED_PROP(float, _Alpha)
 		UNITY_INSTANCING_BUFFER_END(Props)
 
 		void surf (Input IN, inout SurfaceOutput o) {
-            if(_PixelSkip > 0) {
-                //float4 sp = mul(UNITY_MATRIX_VP, float4(IN.worldPos, 1));
-                int2 pixPos = IN.screenPos.xy*_ScreenSize/IN.screenPos.w/_PixelSize;
-                bool dontSkip = (pixPos.x%(int)_PixelSkip) < 1 && (pixPos.y%(int)_PixelSkip) < 1;
-                //bool dontSkip = IN.pixPos.x < .5;
-                dontSkip = dontSkip || ((pixPos.x + ((int)_PixelSkip >> 1))%(int)_PixelSkip) == 0 && ((pixPos.y + ((int)_PixelSkip>>1))%(int)_PixelSkip) == 0;
-                //o.Alpha = dontSkip -1;
-                clip(dontSkip -1);
-            }
+            int2 pixPos = IN.screenPos.xy*_ScreenSize/IN.screenPos.w;
+
+            float4x4 thresholdMatrix =
+            {
+            1.0 / 17.0,  9.0 / 17.0,  3.0 / 17.0, 11.0 / 17.0,
+            13.0 / 17.0,  5.0 / 17.0, 15.0 / 17.0,  7.0 / 17.0,
+            4.0 / 17.0, 12.0 / 17.0,  2.0 / 17.0, 10.0 / 17.0,
+            16.0 / 17.0,  8.0 / 17.0, 14.0 / 17.0,  6.0 / 17.0
+            };
+
+            clip(UNITY_ACCESS_INSTANCED_PROP(_Alpha_arr, _Alpha) - thresholdMatrix[pixPos.x % 4][pixPos.y % 4]);
 
 			o.Albedo = (tex2D (UNITY_ACCESS_INSTANCED_PROP(_MainTex_arr, _MainTex), IN.uv_MainTex) * 
                         UNITY_ACCESS_INSTANCED_PROP(_Color_arr, _Color)).rgb;
