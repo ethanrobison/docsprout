33760deabde00c7074651f1e9800663e46601b18
Ethan Robison <ethancrobison@gmail.com>
2018-04-17 15:50:48 -0500

    respawn on falling

---
+++ Assets/Code/Characters/RespawnOnFall.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Code.Characters
+{
+    public class RespawnOnFall : MonoBehaviour
+    {
+        public Vector3 StartPos;
+        public float Thresh;
+
+        private void Update () {
+            if (transform.position.y < Thresh) { Respawn(); }
+        }
+
+        private void Respawn () { transform.position = StartPos; }
+    }
+}
