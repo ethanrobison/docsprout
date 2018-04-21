6942a13ed064a769c28b6f2dbb6f79ae59ecd3ea
Ethan Robison <ethancrobison@gmail.com>
2018-04-21 12:13:34 -0500

    fix placement...

--- Assets/Code/Characters/Doods/DoodManager.cs
+++ Assets/Code/Characters/Doods/DoodManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using Code.Doods;
+using Code.Utils;
 using UnityEngine;
 
 namespace Code.Characters.Doods
@@ -10,19 +11,19 @@ namespace Code.Characters.Doods
 
         public readonly List<Dood> DoodList = new List<Dood>();
 
-        public void Initialize () {
-            MakeNDoods(5);
-        }
+        public void Initialize () { MakeNDoods(50, new Vector3(-10f, 0f, 20f)); }
 
         public void ShutDown () { }
 
-        private void MakeNDoods (int n, Vector3 startpos = new Vector3()) {
-            for (int i = 0; i < n; i++) {
-                var pos = startpos + new Vector3(n * 1.2f, 5f, (n % 5) * 1.2f);
+        private void MakeNDoods (int count, Vector3 startpos) {
+            const float diff = 1.2f;
+            for (var i = 0; i < count; i++) {
+                var pos = startpos + new Vector3((i % 5) * diff, 8f, -i * diff);
+                Logging.Log(pos.ToString());
                 MakeDood(pos, "Base Dood");
             }
         }
-        
+
         private void MakeDood (Vector3 pos, string name) {
             var prefab = Resources.Load("Doods/" + name);
             var go = (GameObject) Object.Instantiate(prefab, pos, Quaternion.identity);
