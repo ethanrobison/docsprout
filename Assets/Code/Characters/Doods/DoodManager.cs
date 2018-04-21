137151e6729a95e64660fc2a74530d4bd183ceaf
Ethan Robison <ethancrobison@gmail.com>
2018-04-21 11:34:25 -0500

    make N doods

--- Assets/Code/Characters/Doods/DoodManager.cs
+++ Assets/Code/Characters/Doods/DoodManager.cs
@@ -11,17 +11,18 @@ namespace Code.Characters.Doods
         public readonly List<Dood> DoodList = new List<Dood>();
 
         public void Initialize () {
-            // todo what is going on here
-            for (var i = 0; i < 5; i++) {
-                for (float y = 0; y < 12f; y += 1.2f) {
-                    MakeDood(new Vector3(i, 5f, y), "Base Dood");
-                }
-            }
+            MakeNDoods(5);
         }
 
         public void ShutDown () { }
 
-
+        private void MakeNDoods (int n, Vector3 startpos = new Vector3()) {
+            for (int i = 0; i < n; i++) {
+                var pos = startpos + new Vector3(n * 1.2f, 5f, (n % 5) * 1.2f);
+                MakeDood(pos, "Base Dood");
+            }
+        }
+        
         private void MakeDood (Vector3 pos, string name) {
             var prefab = Resources.Load("Doods/" + name);
             var go = (GameObject) Object.Instantiate(prefab, pos, Quaternion.identity);
