5ab92d48537ec8a3b7d45736c67d58b03d2b5686
Ethan Robison <ethancrobison@gmail.com>
2018-04-21 21:45:43 -0500

    move status display to its own file

---
+++ Assets/Code/Characters/Doods/Needs/StatusDisplay.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+
+namespace Code.Characters.Doods.Needs
+{
+    public class StatusDisplay
+    {
+        private readonly GameObject _needMore;
+        private readonly GameObject _needLess;
+
+        public StatusDisplay (GameObject go) {
+            _needMore = go.transform.Find("NeedWater").gameObject;
+            _needLess = go.transform.Find("StopWater").gameObject;
+        }
+
+        public void Show (int status) {
+            // lol this could be a bit manipulation // but m'clarity
+            switch (status) {
+                case 0:
+                    SetDisplays(false, false);
+                    break;
+                case 1:
+                    SetDisplays(false, true);
+                    break;
+                case -1:
+                    SetDisplays(true, false);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public void Hide () { SetDisplays(false, false); }
+
+        private void SetDisplays (bool under, bool over) {
+            _needMore.SetActive(under);
+            _needLess.SetActive(over);
+        }
+    }
+}
