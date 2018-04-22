c7c880ad0932fcc8bab49c40c9086510a7f700ff
Ethan Robison <ethancrobison@gmail.com>
2018-04-21 23:12:51 -0500

    water advertiser lets them shower

--- Assets/Code/Environment/Advertising/WaterAdvertiser.cs
+++ Assets/Code/Environment/Advertising/WaterAdvertiser.cs
@@ -1,11 +1,20 @@
 ﻿using Code.Characters.Doods.Needs;
 using Code.Utils;
+using UnityEngine;
 
 namespace Code.Environment.Advertising
 {
     public class WaterAdvertiser : Advertiser
     {
-        public override void InteractWith (IAdvertisable user) { Logging.Log("Time to shower!"); }
+        public override void InteractWith (IAdvertisable user) {
+            var mono = user as MonoBehaviour;
+            if (mono == null) { return; }
+
+            // todo soft-coded for doods
+            var waterable = mono.GetComponentInParent<Waterable>();
+            if (waterable != null) { waterable.Satisfy(); }
+        }
+
         public override NeedType Satisfies () { return NeedType.Water; }
     }
 }
