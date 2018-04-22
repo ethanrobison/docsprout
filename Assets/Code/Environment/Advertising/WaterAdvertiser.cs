6989551bb18d2884e99fcb5fef0ab6bfb0969a5d
Ethan Robison <ethancrobison@gmail.com>
2018-04-21 22:33:17 -0500

    advertising initial

--- Assets/Code/Environment/Advertising/WaterAdvertiser.cs
+++ Assets/Code/Environment/Advertising/WaterAdvertiser.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections.Generic;
-using Code.Characters.Doods.Needs;
+﻿using Code.Characters.Doods.Needs;
+using Code.Utils;
 
 namespace Code.Environment.Advertising
 {
     public class WaterAdvertiser : Advertiser
     {
-        public override List<Need> GetNeedsSatisfied () { return null; }
+        public override void InteractWith (IAdvertisable user) { Logging.Log("Time to shower!"); }
+        public override NeedType Satisfies () { return NeedType.Water; }
     }
 }
