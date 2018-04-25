e921465faef5ac35cf7ee3ed64bd03c161da24c3
Ethan Robison <ethancrobison@gmail.com>
2018-04-24 19:30:06 -0500

    selector rework initial; triggers acting appropriatelyish

--- Assets/Code/Interaction/Selector.cs
+++ Assets/Code/Interaction/Selector.cs
@@ -1,10 +1,9 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System;
 using Code.Characters.Doods;
 using Code.Characters.Player;
 using Code.Session;
 using Code.Utils;
 using UnityEngine;
-using UnityEngine.Experimental.PlayerLoop;
 
 namespace Code.Interaction
 {
@@ -20,113 +19,79 @@ namespace Code.Interaction
 
         private const float MAX_SPEED = 8f;
         private const float MIN_SPEED = 3f;
+        private const float MIN_DIST = 2f;
+        private const float MAX_DIST = 15f;
         private const float SPEED_CONSTANT = 10f;
 
         public GameObject Cursor;
 
-        public float MinDist = 2f;
-        public float MaxDist = 15f;
+
+        private readonly float[] _sizes = { 0.5f, 1.5f, 2.5f, 4f, 5.5f };
+        private readonly RaycastHit[] _hits = new RaycastHit[50];
 
         private MultiFocusCam _multiFocusCamera;
         private CameraController _cameraController;
+        private Renderer _renderer;
+
+        private Vector3 _pos = new Vector3(0f, 50f, 0f);
         private SelectionMode _mode;
 
-        [SerializeField] private float[] _sizes;
         private int _sizeInd;
-        private RaycastHit[] _hits;
-        private Vector3 _pos;
-
-        private Renderer _renderer;
 
         private float Size {
             get { return _sizes[_sizeInd]; }
         }
 
+
         private void Start () {
             _multiFocusCamera = gameObject.GetRequiredComponent<MultiFocusCam>();
             _cameraController = gameObject.GetRequiredComponent<CameraController>();
-
-            _pos = new Vector3(0f, 50f, 0f);
-            _sizeInd = ((_sizes.Length + 1) >> 1) - 1;
-            _hits = new RaycastHit[50];
-            Cursor.transform.localScale = new Vector3(Size * 2f, Cursor.transform.localScale.y, Size * 2f);
-            Game.Sesh.Input.Monitor.RegisterMapping(ControllerButton.LeftBumper, DecreaseSize);
-            Game.Sesh.Input.Monitor.RegisterMapping(ControllerButton.RightBumper, IncreaseSize);
-            Game.Sesh.Input.Monitor.RegisterMapping(ControllerButton.BButton, () => {
-                if (_mode != SelectionMode.Off) Game.Ctx.Doods.DeselectAll();
-            });
-
-            Game.Sesh.Input.Monitor.RegisterMapping(ControllerButton.YButton, () => {
-                if (_mode == SelectionMode.Off) {
-                    _mode = SelectionMode.Idle;
-                    _renderer.enabled = true;
-                    Vector3 camForward = Vector3.Cross(Camera.main.transform.up, Camera.main.transform.right);
-                    SetPos(-camForward * MinDist);
-                    _cameraController.enabled = false;
-                    _multiFocusCamera.enabled = true;
-                }
-                else {
-                    _mode = SelectionMode.Off;
-                    _renderer.enabled = false;
-                    _cameraController.enabled = true;
-                    _multiFocusCamera.enabled = false;
-                    _multiFocusCamera.RelinquishControl(_cameraController);
-                }
-            });
+            _renderer = Cursor.GetRequiredComponent<Renderer>();
 
             _multiFocusCamera.enabled = false;
             _cameraController.enabled = true;
-
-            _renderer = Cursor.GetComponent<Renderer>();
             _renderer.enabled = false;
+
+            _sizeInd = ((_sizes.Length + 1) >> 1) - 1;
+            Cursor.transform.localScale = new Vector3(Size * 2f, Cursor.transform.localScale.y, Size * 2f);
+
+            // todo a more graceful way to check if the game has started
+            if (Game.Ctx != null) {
+                RegisterMappings();
+            }
         }
 
         private void Update () {
-            if (_mode == SelectionMode.Off) return;
-            if (_mode == SelectionMode.Selecting) {
-                if (Game.Sesh.Input.Monitor.RT < 0.1f) {
-                    _mode = SelectionMode.Idle;
-                }
-            }
-            else if (_mode == SelectionMode.Deselecting) {
-                if (Game.Sesh.Input.Monitor.LT < 0.1f) {
-                    _mode = SelectionMode.Idle;
-                }
-            }
-
-            if (_mode == SelectionMode.Idle) {
+            if (_mode == SelectionMode.Off) {
                 if (Game.Sesh.Input.Monitor.RT > 0.1f) {
-                    _mode = SelectionMode.Selecting;
+                    TransitionToMode(SelectionMode.Selecting);
                 }
                 else if (Game.Sesh.Input.Monitor.LT > 0.1f) {
-                    _mode = SelectionMode.Deselecting;
+                    TransitionToMode(SelectionMode.Deselecting);
                 }
-                else {
-                    return;
-                }
+
+                return;
             }
+
+            var inelegible = !(_mode == SelectionMode.Selecting && Game.Sesh.Input.Monitor.RT > 0.1f ||
+                               _mode == SelectionMode.Deselecting && Game.Sesh.Input.Monitor.LT > 0.1f);
+            if (inelegible) { return; }
 
             int n = Physics.SphereCastNonAlloc(_pos + transform.position + Vector3.up * 50f, Size, Vector3.down,
                 _hits);
             for (int i = 0; i < n; ++i) {
-                Dood dood = _hits[i].transform.GetComponent<Dood>();
-                if (!dood) {
-                    continue;
-                }
-
-                if (_mode == SelectionMode.Selecting) {
-                    dood.OnSelect();
-                }
-                else if (_mode == SelectionMode.Deselecting) {
-                    dood.OnDeselect();
-                }
+                var dood = _hits[i].transform.GetComponent<Dood>();
+                if (dood != null) { HandleDood(dood); }
             }
         }
 
+        private void HandleDood (ISelectable dood) {
+            if (_mode == SelectionMode.Selecting) { dood.OnSelect(); }
+            else if (_mode == SelectionMode.Deselecting) { dood.OnDeselect(); }
+        }
+
         private void FixedUpdate () {
-            if (_mode == SelectionMode.Off) {
-                return;
-            }
+            if (_mode == SelectionMode.Off) { return; }
 
             float curSpeed = _pos.magnitude;
             curSpeed = MIN_SPEED + (MAX_SPEED - MIN_SPEED) * curSpeed / (curSpeed + SPEED_CONSTANT);
@@ -135,21 +100,70 @@ namespace Code.Interaction
             Vector3 move = camRight * Game.Sesh.Input.Monitor.RightH - camForward * Game.Sesh.Input.Monitor.RightV;
             AddPos(move * curSpeed * Time.deltaTime);
             _pos.y = 0f;
-            if (_pos.sqrMagnitude < MinDist * MinDist) {
+            if (_pos.sqrMagnitude < MIN_DIST * MIN_DIST) {
                 _pos.Normalize();
-                _pos *= MinDist;
+                _pos *= MIN_DIST;
             }
             else {
-                _pos = Vector3.ClampMagnitude(_pos, MaxDist);
+                _pos = Vector3.ClampMagnitude(_pos, MAX_DIST);
             }
         }
 
-        public void SetPos (Vector3 pos) {
+        private void RegisterMappings () {
+            // B Button to DeselectAll
+            Game.Sesh.Input.Monitor.RegisterMapping(ControllerButton.BButton, OnBPress);
+
+            // Y Button to PikminWhistle
+//            Game.Sesh.Input.Monitor.RegisterMapping(ControllerButton.YButton, () => {
+//                TransitionToMode(_mode == SelectionMode.Off ? SelectionMode.Idle : SelectionMode.Off);
+//            });
+
+            // RT, RT to select
+            // LT, LT to deselect
+
+            // bumbers to change size
+            Game.Sesh.Input.Monitor.RegisterMapping(ControllerButton.LeftBumper, () => { ChangeSize(-1); });
+            Game.Sesh.Input.Monitor.RegisterMapping(ControllerButton.RightBumper, () => { ChangeSize(1); });
+        }
+
+        private void OnBPress () {
+            if (_mode == SelectionMode.Off) {
+                Game.Ctx.Doods.DeselectAll();
+            }
+            else {
+                TransitionToMode(SelectionMode.Off);
+            }
+        }
+
+        private void TransitionToMode (SelectionMode mode) {
+            switch (mode) {
+                case SelectionMode.Off:
+                    _renderer.enabled = false;
+                    _cameraController.enabled = true;
+                    _multiFocusCamera.enabled = false;
+                    _multiFocusCamera.RelinquishControl(_cameraController);
+                    break;
+                case SelectionMode.Selecting:
+                case SelectionMode.Deselecting:
+                    _renderer.enabled = true;
+                    Vector3 camForward = Vector3.Cross(Camera.main.transform.up, Camera.main.transform.right);
+                    SetPos(-camForward * MIN_DIST);
+                    _cameraController.enabled = false;
+                    _multiFocusCamera.enabled = true;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException("mode", mode, null);
+            }
+
+            _mode = mode;
+        }
+
+        private void SetPos (Vector3 pos) {
             _pos = pos;
             Cursor.transform.position = _pos + transform.position;
         }
 
-        public void AddPos (Vector3 pos) {
+        private void AddPos (Vector3 pos) {
             _pos += pos;
             Cursor.transform.position = _pos + transform.position;
         }
@@ -166,13 +180,8 @@ namespace Code.Interaction
             Cursor.transform.position = _pos + transform.position;
         }
 
-        private void IncreaseSize () {
-            _sizeInd = Mathf.Min(_sizeInd + 1, _sizes.Length - 1);
-            Cursor.transform.localScale = new Vector3(Size * 2f, Cursor.transform.localScale.y, Size * 2f);
-        }
-
-        private void DecreaseSize () {
-            _sizeInd = Mathf.Max(_sizeInd - 1, 0);
+        private void ChangeSize (int delta) {
+            _sizeInd = Mathf.Clamp(_sizeInd + delta, 0, _sizes.Length - 1);
             Cursor.transform.localScale = new Vector3(Size * 2f, Cursor.transform.localScale.y, Size * 2f);
         }
     }
