7cf2429f290a70bb36497457cfc4853145fc0052
Kyle Engelmann <kyleengelmann2018@u.northwestern.edu>
2018-04-28 19:09:23 -0500

    satisfier now one class

---
+++ Assets/Code/environment/Beachball.cs
@@ -0,0 +1,34 @@
+﻿using Code.Characters.Doods;
+using Code.Environment.Advertising;
+using Code.Utils;
+using UnityEngine;
+
+namespace Code.environment
+{
+	public class Beachball : MonoBehaviour {
+	
+		public Vector3 Force = new Vector3(0f, 5f, 5f);
+		
+		private Rigidbody _rig;
+		private float _cooldown;
+		private const float COOLDOWN_TIME = .1f;
+
+		private void Start () {
+			_rig = gameObject.GetRequiredComponent<Rigidbody>();
+			gameObject.GetRequiredComponentInChildren<Satisfier>().OnInteract += Kick;
+		}
+
+		private void Update () {
+			if(_cooldown > 0f) {
+				_cooldown -= Time.deltaTime;
+			}
+		}
+
+
+		private void Kick(Dood dood) {
+			if(_cooldown > 0f) return;
+			_cooldown += COOLDOWN_TIME;
+			_rig.AddForce(dood.transform.TransformVector(Force), ForceMode.VelocityChange);
+		}
+	}
+}
