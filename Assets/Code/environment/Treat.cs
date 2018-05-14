5a4cd13517fcfc0395deb3f94335f6c38635442b
Ethan Robison <ethancrobison@gmail.com>
2018-05-14 16:02:09 -0500

    treat tweaks gdi kyle

--- Assets/Code/environment/Treat.cs
+++ Assets/Code/environment/Treat.cs
@@ -4,23 +4,20 @@ using Code.Characters.Doods;
 
 namespace Code.environment
 {
-	public class Treat : MonoBehaviour
-	{
-		private float _lifetime = 100f;
-		private float _decayRate = 5f;
-		private Satisfier _satisfier;
+    public class Treat : MonoBehaviour
+    {
+        private const float DECAY_RATE = 5f;
+        private float _lifetime = 100f;
+        private Satisfier _satisfier;
 
-		void Start () {
-			_satisfier = GetComponentInChildren<Satisfier>();
-			_satisfier.OnInteract += Snacking;
-		}
+        private void Start () {
+            _satisfier = GetComponentInChildren<Satisfier>();
+            _satisfier.OnInteract += Snacking;
+        }
 
-		private void Snacking (Dood dood) {
-			_lifetime -= _decayRate * Time.deltaTime;
-			if (_lifetime <= 0f) {
-				//_satisfier.OnInteract -= Snacking;
-				Object.Destroy(gameObject);
-			}
-		}
-	}
+        private void Snacking (Dood dood) {
+            _lifetime -= DECAY_RATE * Time.deltaTime;
+            if (_lifetime <= 0f) { Destroy(gameObject); }
+        }
+    }
 }
