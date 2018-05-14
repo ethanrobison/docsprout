b5d4cac021fffb9853b1e0b81d06b40be397d348
Ethan Robison <ethancrobison@gmail.com>
2018-05-14 15:59:45 -0500

    on treat disappear prevent shenanigans

--- Assets/Code/Characters/Doods/AI/Leaves/GoToTreat.cs
+++ Assets/Code/Characters/Doods/AI/Leaves/GoToTreat.cs
@@ -4,19 +4,20 @@ using UnityEngine;
 
 namespace Code.Characters.Doods.AI
 {
-	public class GoToTreat : BehaviorTreeNode
-	{
-		public GoToTreat (Dood dood) : base(dood) {  }
+    public class GoToTreat : BehaviorTreeNode
+    {
+        public GoToTreat (Dood dood) : base(dood) { }
 
-		public override void OnTerminate (Status result) {
-			base.OnTerminate(result);
-			Dood.StopMoving();
-		}
+        public override void OnTerminate (Status result) {
+            base.OnTerminate(result);
+            Dood.StopMoving();
+        }
 
-		protected override Status Update () {
-			var goal = Dood.Comps.Status.TreatLocations[0];
-			return Dood.MoveTowards(goal.position, 3f) ? Status.Success : Status.Running;
-		}
-	}	
+        protected override Status Update () {
+            var goal = Dood.Comps.Status.TreatLocations[0];
+            if (goal == null) { return Status.Failure; }
+
+            return Dood.MoveTowards(goal.position, 3f) ? Status.Success : Status.Running;
+        }
+    }
 }
-
