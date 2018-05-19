e457c9506b1a80af2eae284962b304b8d7b1c88b
Ethan Robison <ethancrobison@gmail.com>
2018-05-19 14:20:26 -0500

    hud tweaks

--- Assets/Code/Characters/Player/HUD.cs
+++ Assets/Code/Characters/Player/HUD.cs
@@ -1,4 +1,7 @@
-﻿using Code.Utils;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Code.Session;
+using Code.Utils;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -19,15 +22,67 @@ namespace Code.Characters.Player
     public class HUD
     {
         private readonly Text _info;
-        private Transform _buttons;
+        private Inventory _inventory;
 
         public HUD () {
             var go = UIUtils.MakeUIPrefab(UIPrefab.HUD);
             _info = UIUtils.FindUICompOfType<Text>(go, "Info/Text");
-            _buttons = go.transform.Find("Buttons");
             Game.Ctx.Economy.Stats.OnFrootChanged += OnFrootChanged;
+
+            _inventory = new Inventory();
+            Game.Sesh.Input.Monitor.RegisterMapping(ControllerButton.LeftBumper, OnLeftPress);
+            Game.Sesh.Input.Monitor.RegisterMapping(ControllerButton.RightBumper, OnRightPress);
+            Game.Sesh.Input.Monitor.RegisterMapping(ControllerButton.XButton, OnDownPress);
         }
 
         private void OnFrootChanged (int froot) { _info.text = string.Format("{0}", froot); }
+
+        private void OnLeftPress () { ChangeActiveItem(-1); }
+
+        private void OnRightPress () { ChangeActiveItem(1); }
+
+        private void OnDownPress () { _inventory.UseItem(); }
+
+        private void ChangeActiveItem (int delta) {
+            Logging.Assert(Mathf.Abs(delta) <= 1, "Invalid delta.");
+            _inventory.ChangeActive(delta);
+        }
+
+
+        private class Inventory
+        {
+            private int _active;
+            private readonly List<ItemInfo> _items = new List<ItemInfo>();
+
+            public void ChangeActive (int delta) {
+                _active += delta;
+                if (_active < 0) { _active = _items.Count - 1; }
+                else if (_active > _items.Count) { _active = 0; }
+
+                Logging.Log(_active);
+            }
+
+            public void UseItem () {
+                Logging.Log(_active);
+                _items[_active].Use();
+            }
+
+            // todo shite
+            public void AddItem (ItemInfo item) {
+                if (_items.Contains(item)) { _items.First(i => i == item).Add(item); }
+                else { _items.Add(item); }
+            }
+        }
+
+        private class ItemInfo
+        {
+            public int Count { get; private set; }
+
+            public string GetInfo () { return ""; }
+
+            public void Use () { }
+
+            public void Add (ItemInfo item) { Count++; }
+        }
     }
 }
